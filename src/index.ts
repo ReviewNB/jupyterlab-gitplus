be935338ff7c75e3a2e9356b25c23b9e06a4e552
Amit Rathi <amit.juschill@gmail.com>
2020-03-05 21:05:31 +0530

    Add Git-Plus menu item

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
-import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { IMainMenu } from '@jupyterlab/mainmenu';
+import { Menu } from '@lumino/widgets';
 
 
-/**
- * Initialization data for the @reviewnb/gitplus extension.
- */
 const extension: JupyterFrontEndPlugin<void> = {
+  requires: [IMainMenu],
   id: '@reviewnb/gitplus',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension @reviewnb/gitplus is activated!');
+  activate: (app: JupyterFrontEnd, mainMenu: IMainMenu) => {
+    console.log('JupyterLab extension @reviewnb/gitplus is activated! - v5');
+
+    // Create new command
+    const commandID = 'create-pr';
+    app.commands.addCommand(commandID, {
+      label: 'Create Pull Request',
+      execute: () => {
+        console.log(`Executed ${commandID}`);
+      }
+    });
+
+    // Create new top level menu
+    const menu = new Menu({ commands: app.commands });
+    menu.title.label = 'Git-Plus';
+    mainMenu.addMenu(menu, { rank: 40 });
+
+    // Add command to menu
+    menu.addItem({
+      command: commandID,
+      args: {},
+    });
+
+
+
   }
 };
 
