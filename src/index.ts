ac560fa3d29ea4b12e3de0d6156baa67265657c4
Amit Rathi <amit.juschill@gmail.com>
2020-03-05 21:22:33 +0530

    Simplify code by decoupling plugin declaration and activate function

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,43 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+//import { Dialog, showDialog } from "@jupyterlab/apputils";
 import { IMainMenu } from '@jupyterlab/mainmenu';
 import { Menu } from '@lumino/widgets';
 
-
-const extension: JupyterFrontEndPlugin<void> = {
+/**
+ * The plugin registration information.
+ */
+const gitPlusPlugin: JupyterFrontEndPlugin<void> = {
+  activate,
   requires: [IMainMenu],
   id: '@reviewnb/gitplus',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd, mainMenu: IMainMenu) => {
-    console.log('JupyterLab extension @reviewnb/gitplus is activated! - v5');
-
-    // Create new command
-    const commandID = 'create-pr';
-    app.commands.addCommand(commandID, {
-      label: 'Create Pull Request',
-      execute: () => {
-        console.log(`Executed ${commandID}`);
-      }
-    });
-
-    // Create new top level menu
-    const menu = new Menu({ commands: app.commands });
-    menu.title.label = 'Git-Plus';
-    mainMenu.addMenu(menu, { rank: 40 });
-
-    // Add command to menu
-    menu.addItem({
-      command: commandID,
-      args: {},
-    });
-
-
-
-  }
+  autoStart: true
 };
 
-export default extension;
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterFrontEnd, mainMenu: IMainMenu) {
+  console.log('JupyterLab extension @reviewnb/gitplus is activated! - v6');
+  // Create new command
+  const commandID = 'create-pr';
+  app.commands.addCommand(commandID, {
+    label: 'Create Pull Request',
+    execute: () => {
+      console.log(`Executed ${commandID}`);
+    }
+  });
+
+  // Create new top level menu
+  const menu = new Menu({ commands: app.commands });
+  menu.title.label = 'Git-Plus';
+  mainMenu.addMenu(menu, { rank: 40 });
+
+  // Add command to menu
+  menu.addItem({
+    command: commandID,
+    args: {},
+  });
+};
+
+
+export default gitPlusPlugin;
