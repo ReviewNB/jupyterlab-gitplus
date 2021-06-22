4ff97a5541db5b84e94ad1ed60808c1c12c68e33
Amit Rathi <amit.juschill@gmail.com>
2021-06-22 08:28:18 +0530

    Custom CSS to be compatiable with both JL2.0 and 3.0

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ function activate(
   notebookTracker: INotebookTracker
 ) {
   console.log(
-    'JupyterLab extension @reviewnb/gitplus (0.1.4) is activated!'
+    'JupyterLab extension @reviewnb/gitplus (0.1.5) is activated!'
   );
   const createPRCommand = 'create-pr';
   app.commands.addCommand(createPRCommand, {
