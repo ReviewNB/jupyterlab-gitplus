3530952d236abdc6ecd9d47affe12a17fa3217dd
Amit Rathi <amit.juschill@gmail.com>
2021-07-04 10:28:37 +0530

    Update setup.py to be comatible with JL3.0

--- setup.py
+++ setup.py
@@ -15,8 +15,26 @@ version = re.search(
 with open("README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
 
+HERE = Path(__file__).parent.resolve()
 
-setup(
+# The name of the project
+name = "jupyterlab_gitplus"
+
+
+lab_path = (HERE / name.replace("-", "_") / "labextension")
+labext_name = "@reviewnb/jupyterlab_gitplus"
+
+data_files_spec = [
+    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path), "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, str(HERE), "install.json"),
+    ("etc/jupyter/jupyter_server_config.d",
+     "jupyter-config/server-config", "jupyterlab_gitplus.json"),
+    # For backward compatibility with notebook server
+    ("etc/jupyter/jupyter_notebook_config.d",
+     "jupyter-config/nb-config", "jupyterlab_gitplus.json"),
+]
+
+setup_args = dict(
     name = "jupyterlab_gitplus",
     packages = ["jupyterlab_gitplus"],
     python_requires='>=3',
@@ -28,6 +46,8 @@ setup(
     author_email = "amit@reviewnb.com",
     url = "https://github.com/ReviewNB/jupyterlab-gitplus",
     keywords=['github', 'jupyter', 'notebook', 'pull request', 'version control', 'git'],
+    include_package_data=True,
+    platforms="Linux, Mac OS X, Windows",
     install_requires=[
         'jupyterlab',
         'gitpython',
@@ -35,3 +55,21 @@ setup(
         'urllib3'
     ]
 )
+
+
+try:
+    from jupyter_packaging import (
+        wrap_installers,
+        npm_builder,
+        get_data_files
+    )
+    post_develop = npm_builder(
+        build_cmd="install:extension", source_dir="src", build_dir=lab_path
+    )
+    setup_args['cmdclass'] = wrap_installers(post_develop=post_develop)
+    setup_args['data_files'] = get_data_files(data_files_spec)
+except ImportError as e:
+    pass
+
+if __name__ == "__main__":
+    setup(**setup_args)
