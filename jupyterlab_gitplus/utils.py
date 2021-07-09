80d7cef99329cf154746079627c164380d3cd872
Amit Rathi <amit.juschill@gmail.com>
2021-07-09 16:40:29 +0530

    Handle non-bare repository remote URLs

--- jupyterlab_gitplus/utils.py
+++ jupyterlab_gitplus/utils.py
@@ -6,6 +6,10 @@ GITHUB_REMOTE_URL_REGEX = re.compile(r"github\.com\/(.*?)\/(.*?)\.git")
 def get_owner_login_and_repo_name(repo):
     owner_login, repo_name = '', ''
     remote_url = repo.remotes.origin.url
+
+    if not remote_url.endswith('.git'):
+        remote_url += '.git'
+
     match = GITHUB_REMOTE_URL_REGEX.search(remote_url)
 
     if match:
